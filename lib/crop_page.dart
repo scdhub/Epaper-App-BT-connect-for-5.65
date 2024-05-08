4e7a287c5fb130f510adba44a63750c4afdf7865
SCD20200908PC\SCD20200908PC <furukane.yusuke@scd.co.jp>
2024-05-08 11:13:41 +0900

    クロップ用画面追加

---
+++ lib/crop_page.dart
@@ -0,0 +1,249 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:crop/crop.dart';
+import 'dart:ui' as ui;
+
+import 'select-photo-check_page.dart';
+
+// import 'importPreview_page.dart';
+// import 'trimming_cropSpace.dart';
+
+class TrimmingPage extends StatefulWidget {
+  final Uint8List? imageData;
+
+  TrimmingPage({Key? key, required this.imageData}) : super(key: key);
+
+  @override
+  State<TrimmingPage> createState() => _TrimmingPageState();
+}
+
+class _TrimmingPageState extends State<TrimmingPage> {
+  final controller = CropController(aspectRatio: 1.0);//切り取る画像の縦横比1:1に設定
+  bool isSquare = true;
+  void _changeAspectRatio(bool isSquare) {
+    setState(() {
+      controller.aspectRatio = isSquare ? 1.0 : 600 / 448; // 例として16:9を使用
+    });
+  }
+
+  void _cropImage() async {
+    //デバイスのピクセル比を取得して、画面解像度に合わせて画像を切り取る
+    final pixelRatio = MediaQuery.of(context).devicePixelRatio;
+    //指定されたピクセル比で画像を切り取る
+    final ui.Image? cropped = await controller.crop(pixelRatio: pixelRatio);
+    if (cropped == null) {
+      return;
+    }
+    if (!mounted) {//ウィジェットが画面から消えている状態で、Stateを更新しようとするエラーを防ぐ。
+      return;
+    }
+    final byteData = await cropped.toByteData(format: ui.ImageByteFormat.png);
+
+    // bufferがbyteDataのnullの確認が必要のため、
+    if (byteData == null) {
+      return;
+    }
+    final Uint8List cropByte = byteData.buffer.asUint8List();
+
+    var cropBytes = [cropByte];
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(
+    //       builder: (context) => importPreview_page(
+    //           imageBytes: cropBytes)),
+    // );
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SelectCheck(imageData: cropBytes),
+      ),
+    );
+  }
+  // bool light = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.of(context).pop(),
+          ),
+          title: Text('トリミング画面', style: TextStyle(color: Colors.white)),
+          actions: [
+            Row(children: [
+              Switch(
+                  activeColor:Colors.red,
+                value: isSquare,
+              onChanged: (bool value){
+                _changeAspectRatio(value);
+                setState(() {
+                  isSquare = value;
+
+                });
+              },
+              ),
+            IconButton(
+              icon: Icon(Icons.done, color: Colors.white),
+              onPressed: _cropImage,
+            ),
+      ]),
+          ],
+          backgroundColor: Colors.black45,
+        ),
+        backgroundColor: Colors.black45,
+        body:
+    // AspectRatio(
+    //       aspectRatio:600/448,
+    // child:
+    CropSpace(
+          imageData: widget.imageData!,
+          controller: controller,
+        ),
+      // ),
+      ),
+    );
+  }
+}
+
+
+Widget buildGridHelper(double aspectRatio) {
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      // アスペクト比に基づいてグリッドの幅と高さを計算
+      final double gridWidth = constraints.maxWidth / 3;
+      final double gridHeight = (constraints.maxWidth / aspectRatio) / 3;
+
+      return Stack(
+        children: <Widget>[
+          // 横線
+          for (int i = 0; i < 3; i++)
+            Positioned(
+              left: 0,
+              top: gridHeight * i,
+              child: Container(
+                width: constraints.maxWidth,
+                height: 1,
+                color: Colors.white,
+              ),
+            ),
+          // 縦線
+          for (int i = 0; i < 3; i++)
+            Positioned(
+              left: gridWidth * i,
+              top: 0,
+              child: Container(
+                width: 1,
+                height: constraints.maxHeight,
+                color: Colors.white,
+              ),
+            ),
+        ],
+      );
+    },
+  );
+}
+
+// Widget buildGridHelper() {
+//   return LayoutBuilder(
+//     builder: (context, constraints) {
+//       final double gridWidth = constraints.maxWidth / 3;
+//       final double gridHeight = constraints.maxHeight / 3;
+//       // final double gridWidth = constraints.maxWidth / 3;
+//       // final double gridHeight = (constraints.maxWidth / aspectRatio) / 3;
+//
+//       return Stack(
+//         children: <Widget>[
+//           for (int i = 0; i < 3; i++)
+//             Positioned(
+//               left: 0,
+//               top: gridHeight * i,
+//               child: Container(
+//                 width: constraints.maxWidth,
+//                 height: 2,
+//                 color: Colors.black,
+//               ),
+//             ),
+//           for (int i = 0; i < 3; i++)
+//             Positioned(
+//               left: gridWidth * i,
+//               top: 0,
+//               child: Container(
+//                 width: 2,
+//                 height: constraints.maxHeight,
+//                 color: Colors.black,
+//               ),
+//             ),
+//         ],
+//       );
+//     },
+//   );
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:crop/crop.dart';
+// import 'dart:typed_data';
+//
+// import 'trimming_cropSpace_GridHelper.dart';
+
+class CropSpace extends StatefulWidget {
+  final Uint8List imageData;
+  final CropController controller;
+
+  CropSpace({required this.imageData, required this.controller});
+
+  @override
+  _CropSpaceState createState() => _CropSpaceState();
+}
+
+class _CropSpaceState extends State<CropSpace> {
+  double _rotation = 0; // 画像の回転角度
+  BoxShape shape = BoxShape.rectangle;//切り取る形状、四角形
+  // final double aspectRatio = getAspectRatio();
+  double getAspectRatio() {
+    return widget.controller.aspectRatio;
+  }
+  @override
+  Widget build(BuildContext context) {
+    final double aspectRatio = getAspectRatio();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child:
+            // AspectRatio(
+            //       aspectRatio:600/448,
+            // child:
+            Container(
+              color: Colors.black,
+              padding: const EdgeInsets.all(8),
+              child: Crop(
+                onChanged: (decomposition) {
+                  if (_rotation != decomposition.rotation) {
+                    setState(() {
+                      _rotation = ((decomposition.rotation + 180) % 360) - 180;
+                    });
+                  }
+                },
+                controller: widget.controller,
+                shape: shape,
+                // helper: buildGridHelper(),//切り取り範囲を視覚的に示すための補助線,Grid(3×3)
+                helper: buildGridHelper(aspectRatio),
+                child: Image.memory(
+                  widget.imageData,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          // ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/crop_photo-select.dart
@@ -0,0 +1,436 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+import 'package:transparent_image/transparent_image.dart';
+// import 'appserver.dart';
+import 'crop_page.dart';
+// import 'photo-select_name-select.dart';
+// import 'photo-select_image-list.dart';
+import 'package:photo_manager_image_provider/photo_manager_image_provider.dart';
+// import 'select-photo-check_page.dart';
+
+class Media {
+  final AssetEntity assetEntity;
+  final Widget widget;
+  Media({
+    required this.assetEntity,
+    required this.widget,
+  });
+}
+
+class CropImageSelect_Album extends StatefulWidget {
+
+
+  @override
+  _CropImageSelectAlbumState createState() => _CropImageSelectAlbumState();
+}
+
+class _CropImageSelectAlbumState extends State<CropImageSelect_Album> {
+
+  final ScrollController _scrollController = ScrollController();
+  AssetPathEntity? _currentAlbum;
+  List<AssetPathEntity> _albums = [];
+  // List<AssetEntity> _albums = [];
+  final List<Media> _medias = [];
+  int _lastPage = 0;
+  int _currentPage = 0;
+  List<AssetPathEntity> albums = [];
+  final List<Media> _selectedMedias = [];
+  final List<Media> selectedMedias =[];
+  @override
+  void initState() {
+    super.initState();
+    _selectedMedias.addAll(selectedMedias);
+    _loadAlbums();
+    _scrollController.addListener(_loadMoreMedias);
+  }
+  @override
+  void dispose() {
+    _scrollController.removeListener(_loadMoreMedias);
+    _scrollController.dispose();
+    super.dispose();
+  }
+  void _loadAlbums() async {
+    final permitted = await PhotoManager.requestPermissionExtend();
+    if (permitted.isAuth) {
+      // albums = await fetchAlbums();
+      albums = await PhotoManager.getAssetPathList(
+        // onlyAll: false,
+        type: RequestType.image,
+      );//アルバムリスト取得
+      // _fetchPhotos(albums.first);
+      // _currentAlbum(albums.first);
+      if(albums.isNotEmpty){
+        _currentAlbum = albums.first;
+        _albums = albums;
+        _loadMedias();
+      }
+
+    } else {
+      PhotoManager.openSetting();//許可がない時設定を開く
+    }
+  }
+  // _fetchPhotos(AssetPathEntity album) async {
+  //   // final photoList = await album.getAssetListPaged(page: 0, size: 100);//今のところ100枚の画像を取得するようにしている。後ほどすべて抜き出せるように修正。
+  //   // setState(() {
+  //   //   _currentAlbum = album
+  //   // });
+  //
+  //   // setState(() {
+  //   //   _albums = photoList;
+  //   // });
+  // }
+  //   if (albums.isNotEmpty) {
+  //     setState(() {
+  //       // Set the first album as the current album
+  //       _currentAlbum = albums.first;
+  //       // Update the list of albums
+  //       _albums = albums;
+  //     });
+  //     // Load media items for the current album
+  //     _loadMedias();
+  //   }
+  // }
+
+  // Method to load media items asynchronously
+  void _loadMedias() async {
+    // Store the current page as the last page
+    _lastPage = _currentPage;
+    if (_currentAlbum != null) {
+      // Fetch media items for the current album
+      List<Media> medias =
+      await fetchMedias(album: _currentAlbum!, page: _currentPage);
+      setState(() {
+        // Add fetched media items to the list
+        _medias.addAll(medias);
+      });
+    }
+  }
+
+  // Method to load more media items when scrolling
+  void _loadMoreMedias() {
+    if (_scrollController.position.pixels /
+        _scrollController.position.maxScrollExtent >
+        0.33) {
+      // Check if scrolled beyond 33% of the scroll extent
+      if (_currentPage != _lastPage) {
+        // Load more media items
+        _loadMedias();
+      }
+    }
+  }
+
+  // Method to select or deselect a media item
+  void _selectMedia(Media media) {
+    bool isSelected = _selectedMedias.any((element) =>
+    element.assetEntity.id ==
+        media.assetEntity.id); // Check if the media item is already selected
+    setState(() {
+      if (isSelected) {
+        // Deselect the media item if already selected
+        _selectedMedias.removeWhere(
+                (element) => element.assetEntity.id == media.assetEntity.id);
+      } else {
+        // Select the media item if not already selected
+        _selectedMedias.add(media);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // toolbarTextStyle: TextStyle(color:Colors.white),
+        title: DropdownButton<AssetPathEntity>(
+          borderRadius: BorderRadius.circular(16.0),
+          value: _currentAlbum,
+          items: _albums
+              .map(
+                (e) => DropdownMenuItem<AssetPathEntity>(
+              value: e,
+              // Display album name in dropdown
+              child: Text(e.name.isEmpty ? "0" : e.name),
+            ),
+          )
+              .toList(),
+          onChanged: (AssetPathEntity? value) {
+            setState(() {
+              // Set the selected album as the current album
+              _currentAlbum = value;
+              // Reset current page to load from the beginning
+              _currentPage = 0;
+              // Reset last page
+              _lastPage = 0;
+              // Clear existing media items
+              _medias.clear();
+            });
+            // Load media items for the selected album
+            _loadMedias();
+            // Scroll to the top
+            _scrollController.jumpTo(0.0);
+          },
+        ),
+
+        backgroundColor: Colors.blue,
+      ),
+      body: MediasGridView(
+        // Pass the list of media items to the grid view
+        medias: _medias,
+        // Pass the list of selected media items to the grid view
+        selectedMedias: _selectedMedias,
+        // Pass the method to select or deselect a media item
+        selectMedia: _selectMedia,
+        // Pass the scroll controller to the grid view
+        scrollController: _scrollController,
+
+        // albums:albums,
+      ),
+      floatingActionButton: _selectedMedias.isEmpty
+          ? null
+          : FloatingActionButton(
+        // Close the screen and pass selected media items back
+        // onPressed: () => Navigator.push(context,
+        //
+        //     MaterialPageRoute(builder: (context) =>
+        //         SelectCheck(imageData: _selectedMedias))),
+        onPressed: () async{
+          // Future<Map<String,dynamic>> getMetadata(AssetEntity asset) async{
+          //   // Map<String,dynamic> metadata = await asset.metadata;
+          //   Map<String,dynamic> metadata = {
+          //     'creationDate': asset.createDateTime,
+          //     'width': asset.width,
+          //     'height': asset.height,
+          //     'title': asset.title,
+          //   };
+          //   debugPrint('Metadata: $metadata');
+          //   return metadata;
+          // }
+
+          // debugPrint('Metadata: $metadata');
+          // someFunction();
+          // debugPrint('$metadata');
+          // List<Uint8List?> imageDataList = [];
+          Uint8List? cropImageData ;
+          List<String?> imageMetaData = [];
+          for (Media media in _selectedMedias){
+
+            //   var metadataT = await getMetadata(media.assetEntity);
+            //   debugPrint('Metadata: $metadataT');
+            // imageMetaData.add(metadataT);
+
+            Uint8List? data = await getMediaData(media.assetEntity);
+            if(data != null){
+              // imageDataList(data);
+              setState(() {
+                cropImageData = data;
+              });
+
+            }
+          }
+          // debugPrint();
+
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => TrimmingPage(imageData: cropImageData),
+            ),
+          );
+        },
+
+        backgroundColor: Colors.lightGreenAccent,
+        // Display check icon
+        child: const Icon(Icons.add_task_outlined),
+      ),
+    );
+  }
+}
+
+Future<Uint8List?> getMediaData(AssetEntity assetEntity) async {
+  return await assetEntity.originBytes;
+}
+
+// Function to fetch media items from a specific album and page
+Future<List<Media>> fetchMedias({
+  required AssetPathEntity album,
+  required int page,
+}) async {
+  List<Media> medias = [];
+
+  try {
+    // Get a list of asset entities from the specified album and page
+    final List<AssetEntity> entities =
+    await album.getAssetListPaged(page: 0, size: 30);
+
+    // Loop through each asset entity and create corresponding Media objects
+    for (AssetEntity entity in entities) {
+      // Assign the asset entity to the Media object
+      Media media = Media(
+        assetEntity: entity,
+        // Create a FadeInImage widget to display the media thumbnail
+        widget: FadeInImage(
+          // Placeholder image
+          placeholder: MemoryImage(kTransparentImage),
+          // Set the fit mode to cover
+          fit: BoxFit.cover,
+          // Use AssetEntityImageProvider to load the media thumbnail
+          image: AssetEntityImageProvider(
+            entity,
+            // Thumbnail size
+            thumbnailSize: const ThumbnailSize.square(300),
+            // Load a non-original (thumbnail) image
+            isOriginal: false,
+          ),
+        ),
+      );
+
+      // Media media = Media(
+      //   assetEntity: entity,
+      //   widget: imageWidget,
+      // );
+      // Add the created Media object to the list
+      medias.add(media);
+    }
+
+
+  } catch (e) {
+    // Handle any exceptions that occur during fetching
+    debugPrint('Error fetching media: $e');
+  }
+
+  // Return the list of fetched media items
+  return medias;
+}
+
+// Widget to display a media item with optional selection overlay
+class MediaItem extends StatelessWidget {
+  // The media to display
+  final Media media;
+
+  // Indicates whether the media is selected
+  final bool isSelected;
+
+  // Callback function when the media is tapped
+  final Function selectMedia;
+
+  // Unique identifier for the widget, passes the key to the super constructor
+  const MediaItem({
+    required this.media,
+    required this.isSelected,
+    required this.selectMedia,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      // Callback function when the media is tapped
+      onTap: () => selectMedia(media),
+      child: Stack(
+        children: [
+          //     // Display the media widget with optional padding
+          _buildMediaWidget(),
+          //     // Display the selected overlay if the media is selected
+          if (isSelected) _buildIsSelectedOverlay(),
+        ],
+      ),
+    );
+  }
+
+  // Build the media widget with optional padding
+  Widget _buildMediaWidget() {
+    return Positioned.fill(
+      child: Padding(
+        padding: EdgeInsets.all(isSelected ? 10.0 : 0.0),
+        // Display the media widget
+        child: media.widget,
+      ),
+    );
+  }
+
+  // Build the selected overlay
+  Widget _buildIsSelectedOverlay() {
+    return Positioned.fill(
+      child: Container(
+        // Semi-transparent black overlay
+        color: Colors.black.withOpacity(0.1),
+        child: const Center(
+          child: Icon(
+            // Checkmark icon
+            Icons.check_circle_rounded,
+            // White color for the icon
+            color: Colors.white,
+            // Size of the icon
+            size: 30,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Widget to display a grid of media items
+class MediasGridView extends StatelessWidget {
+  // List of all media items
+  final List<Media> medias;
+
+  // List of selected media items
+  final List<Media> selectedMedias;
+
+  // Callback function to select a media item
+  final Function(Media) selectMedia;
+
+  // Controller for scrolling
+  final ScrollController scrollController;
+
+  // final List<AssetPathEntity> albums;
+
+  const MediasGridView({
+    // Unique identifier for the widget
+    super.key,
+    // List of all media items
+    required this.medias,
+    // List of selected media items
+    required this.selectedMedias,
+    // Callback function to select a media item
+    required this.selectMedia,
+    // Controller for scrolling
+    required this.scrollController,
+
+    // required this.albums
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 3.0),
+      child: GridView.builder(
+        // Assign the provided scroll controller
+        controller: scrollController,
+        // Apply bouncing scroll physics
+        physics: const BouncingScrollPhysics(),
+        // Set the number of items in the grid
+        itemCount: medias.length,
+        // 3 columns in the grid
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          // Spacing between rows
+          mainAxisSpacing: 4,
+          // Spacing between columns
+          crossAxisSpacing: 3,
+        ),
+        // Build each media item using the MediaItem widget
+        itemBuilder: (context, index) => MediaItem(
+          // Pass the current media item
+          media: medias[index],
+          // Check if the media item is selected
+          isSelected: selectedMedias.any((element) =>
+          element.assetEntity.id == medias[index].assetEntity.id),
+          // Pass the selectMedia callback function
+          selectMedia: selectMedia,
+        ),
+      ),
+    );
+  }
+}
