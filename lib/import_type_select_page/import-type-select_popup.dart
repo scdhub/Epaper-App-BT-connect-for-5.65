815fbd31daed21bae775ac947ea3e0d842fc55e1
FurukaneYusuke <furukane.yusuke@scd.co.jp>
2024-06-18 15:52:14 +0900

    不要コメント削除

--- lib/import_type_select_page/import-type-select_popup.dart
+++ lib/import_type_select_page/import-type-select_popup.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import '../crop_page/crop_photo-select.dart';
-import 'import-type-select_radiobutton.dart';
 import '../server_upload/photo-select_page.dart';
 import '../take-photo_page.dart';
-import '../server_upload/select-photo-check_page.dart';
-import 'dart:io';
-import 'dart:typed_data';
-import 'package:image_picker/image_picker.dart';
 
-class ImageTypeSelection_popup extends StatefulWidget {//top_pageから遷移
+//top_pageから遷移
+class ImageTypeSelection_popup extends StatefulWidget {
   const ImageTypeSelection_popup({super.key});
 
   @override
@@ -17,47 +13,26 @@ class ImageTypeSelection_popup extends StatefulWidget {//top_pageから遷移
 }
 
 class _ImageTypeSelection_popupState extends State<ImageTypeSelection_popup> {
-  var _selectedIndex = 1; // ラジオボタンの選択値を親ウィジェットで管理する
+  // var _selectedIndex = 1; // ラジオボタンの選択値を親ウィジェットで管理する
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       actionsAlignment: MainAxisAlignment.center,
       backgroundColor:Colors.white,
-      title:
-      // Text('スマホから画像を\nインポート方法選択',
-      Text('方法選択',
+      title: Text('方法選択',
         textAlign: TextAlign.center,
       ),
-      // content: TypeSelectedRadio(
-      //   // コールバック関数を渡す
-      //   onSelected: (value) {
-      //     setState(() {
-      //       _selectedIndex = value;
-      //     });
-      //   },
-      // ),
       actions: <Widget>[
-    //     content: Container(
-    //     width: MediaQuery.of(context).size.width, // 画面幅の90%に設定
-    // child: Column(
-    // children: [
-    //   Text('スマホから画像を\nインポート方法選択',
-    //     textAlign: TextAlign.center,
-    //   ),
         Column(
     children: [
 Row(
-
-    // mainAxisAlignment: MainAxisAlignment.center,
     mainAxisAlignment: MainAxisAlignment.spaceAround,
     children:[
 
       Container(
-        // color: Colors.greenAccent,
         width: 140,
         height: 100,
-        // padding: ,
         child:
 
         ElevatedButton(
@@ -68,6 +43,7 @@ Row(
                 borderRadius: BorderRadius.circular(10.0)),
           ),
           onPressed: () {
+            // スマホのカメラ機能を使って、写真を撮る
             getImageFromCamera(context);
           },
           child:
@@ -75,15 +51,9 @@ Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-        // IconButton(
-        //   icon:
           Icon(
             Icons.camera_alt_outlined,
             size: 40.0,),
-        //   onPressed:(){
-        //     getImageFromCamera(context);
-        // },
-        // ),
           Text(
             ' 撮影して\n写真を登録',
           ),
@@ -92,11 +62,9 @@ Row(
       ),
 
       Container(
-        // color: Colors.cyan,
         width: 140,
         height: 100,
         child:
-
         ElevatedButton(
           style: TextButton.styleFrom(
             foregroundColor: Colors.black,
@@ -105,6 +73,7 @@ Row(
                 borderRadius: BorderRadius.circular(10.0)),
           ),
           onPressed: () {
+            // アルバムの画像を選択する画面に遷移
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => ImageSelect_Album()),
@@ -115,28 +84,18 @@ Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-        // IconButton(
-        //   icon:
         Icon(
               Icons.photo_library_outlined,
             size: 40.0,
           ),
-          // onPressed:(){
-          //   Navigator.push(
-          //     context,
-          //     MaterialPageRoute(builder: (context) => ImageSelect_Album()),
-          //   );
-          // },),
             Text(
               'アルバムから\n　画像選択',
             ),
           ]),
       ),
       ),
-      // VerticalDivider(/*区切り線の設定*/),
     ]),
       Container(
-        // color: Colors.pinkAccent,
         width: 145,
         height: 100,
           child:
@@ -148,6 +107,7 @@ Row(
                   borderRadius: BorderRadius.circular(10.0)),
             ),
             onPressed: () {
+              //　画像選択して、トリミング画面に遷移する
                     Navigator.push(
                       context,
                       MaterialPageRoute(builder: (context) => CropImageSelect_Album()),
@@ -158,19 +118,10 @@ Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-      // IconButton(
-      //   icon:
       Icon(
           Icons.cut_outlined,
           size: 40.0,
         ),
-    //     onPressed:(){
-    //       Navigator.push(
-    //         context,
-    //         MaterialPageRoute(builder: (context) => CropImageSelect_Album()),
-    //       );
-    //     },
-    // ),
       Text(
         'アルバムから\n1枚トリミング',
       ),
@@ -178,8 +129,6 @@ Row(
       ),
       ),
         ]),
-
-
         Divider(),
         Container(
           width: 300,
@@ -187,127 +136,16 @@ Row(
         child:
         TextButton(
           onPressed: () => Navigator.pop(context, 'Cancel'),
-          child: const Text('キャンセル',style: TextStyle(fontSize:20,color:Colors.black),),
+          child: const Text('キャンセル',
+            style: TextStyle(
+                fontSize:20,
+                color:Colors.black
+            ),
+          ),
         ),
         ),
-        // TextButton(
-        //   onPressed: () {
-        //     if (_selectedIndex == 1) {
-        //       // getImageCamera();
-        //       getImageFromCamera(context);
-        //       // navigateToSelectCheck(context);
-        //       // Navigator.push(
-        //       //   context,
-        //       //   MaterialPageRoute(builder: (context) =>
-        //       //       AlbumImport()),
-        //       // );
-        //     } else {
-        //       Navigator.push(
-        //         context,
-        //         MaterialPageRoute(builder: (context) => imageSelect_Album()),
-        //       );
-        //     }
-        //   },
-        //   child: const Text('OK'),
-        // ),
       ],
-    // ),
-    // ),
     );
   }
 }
 
-// class getImage {
-//   File? image; // 画像を保存する変数
-//
-//   final picker = ImagePicker(); // ImagePickerのインスタンス
-//   Uint8List? byteImage;
-//
-// // カメラから画像を取得するメソッド
-//   Future getImageFromCamera(BuildContext context) async {
-//     final pickedFile = await picker.pickImage(source: ImageSource.camera);
-//     if (pickedFile != null) {
-//       image = File(pickedFile.path);
-//       //トリミング画面にバイトデータとして送る為に変換
-//       byteImage = await image!.readAsBytes();
-//     } else {
-//       print('No image selected.');
-//       return;
-//     }
-//     // プログレスインジケータを表示するダイアログを表示
-//     showDialog(
-//       context: context,
-//       useRootNavigator: false,
-//       builder: (context) {
-//         return Container(
-//             alignment: Alignment.center,
-//             child: SizedBox(
-//                 width: 50,
-//                 height: 50,
-//                 child: ColoredBox(
-//                     color: Colors.white54,
-//                     child: Padding(
-//                         padding: EdgeInsets.all(10.0),
-//                         child: CircularProgressIndicator(
-//                           color: Colors.green,
-//                         )))));
-//       },
-//     );
-//     // 撮影した写真をトリミング画面渡して遷移する
-//     // await
-//     // Navigator.push(
-//     //   context,
-//     //   MaterialPageRoute(
-//     //     builder: (context) => trimming_page(imageData: byteImage!),
-//     //   ),
-//     // );
-//     // Navigator.of(context).pop();
-//   }
-// }
-
-// void getImageCamera(BuildContext context) {
-//   File? image; // 画像を保存する変数
-//   final picker = ImagePicker(); // ImagePickerのインスタンス
-//   Uint8List? byteImage;
-//
-// // カメラから画像を取得するメソッド
-//   Future getImageFromCamera() async {
-//     final pickedFile = await picker.pickImage(source: ImageSource.camera);
-//     if (pickedFile != null) {
-//       image = File(pickedFile.path);
-//       //トリミング画面にバイトデータとして送る為に変換
-//       byteImage = await image!.readAsBytes();
-//       await Navigator.push(
-//         context,
-//         MaterialPageRoute(
-//           builder: (context) => SelectCheck(),
-//         ),
-//       );
-//     } else {
-//       return;
-//     }
-//   }
-// }
-
-
-//   File? image; // 画像を保存する変数
-//   final picker = ImagePicker(); // ImagePickerのインスタンス
-//   Uint8List? byteImage;
-//
-// // カメラから画像を取得するメソッド
-//   Future getImageFromCamera(BuildContext context) async {
-//     final pickedFile = await picker.pickImage(source: ImageSource.camera);
-//     if (pickedFile != null) {
-//       image = File(pickedFile.path);
-//       //トリミング画面にバイトデータとして送る為に変換
-//       byteImage = await image!.readAsBytes();
-//       await Navigator.push(
-//         context,
-//         MaterialPageRoute(
-//           builder: (context) => SelectCheck(),
-//         ),
-//       );
-//     } else {
-//       return;
-//     }
-//   }
