399629df093a04cf1d4ba58ec2d956c8d3d79914
FurukaneYusuke <furukane.yusuke@scd.co.jp>
2024-06-18 16:03:04 +0900

    アプリ全体に書体を反映、appBarの色を描く画面に反映するよう修正。

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'top_page/top_page.dart';
 
 Future<void> main() async{
-  await dotenv.load(fileName: '.env');
+  await dotenv.load(fileName: '.env');//環境変数を使う為、設置。
   runApp(const MyApp());
 }
 
@@ -15,10 +15,18 @@ class MyApp extends StatelessWidget {
       title: 'E ink E-paper',
       theme: ThemeData(
         useMaterial3: true,
+        //アプリ全体に下記フォントを反映
         fontFamily:'NotoSansJP',
+        //アプリ全体のappBarに下記フォント、背景を反映
         appBarTheme: AppBarTheme(
-        color: Color(0xFF87ff99),
-          titleTextStyle: TextStyle(color: Colors.black,fontSize: 20,fontFamily:'NotoSansJP',)),
+            // color: Color(0xFF87ff99),
+            color: Colors.black87,
+          iconTheme: IconThemeData(color: Colors.yellow[100]),
+          actionsIconTheme: IconThemeData(color: Colors.yellow[100]),
+          titleTextStyle: TextStyle(color: Colors.yellow[100],fontSize: 25,fontFamily:'NotoSansJP',),
+        centerTitle: true,
+        toolbarTextStyle: TextStyle(color:Colors.yellow[100],fontSize:20,fontFamily: 'NotoSansJP')
+        ),
       ),
       home: TopPage(title: 'E ink E-paper'),
     );
