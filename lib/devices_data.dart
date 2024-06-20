e80aa44a68ade363e085bf157a9f510d1d696c8e
FurukaneYusuke <furukane.yusuke@scd.co.jp>
2024-06-20 13:02:23 +0900

    不要部分削除

--- lib/devices_data.dart
+++ lib/devices_data.dart
@@ -16,21 +16,3 @@ class ScanDevice{
   ScanDevice({required this.scanName, required this.scanIpAddress,required this.scanDevicesData});
 }
 
-// class DetectDevice {
-//   final String detectName;
-//   final String detectIpAddress;
-//
-//   DetectDevice({required this.detectName , required this.detectIpAddress});
-// }
-//
-// List<DetectDevice> devices = [
-//   DetectDevice(detectName:'Device 1',detectIpAddress: '192,168,1.1'),
-//   DetectDevice(detectName:'Device 2',detectIpAddress: '192,168,1.2'),
-//   DetectDevice(detectName:'Device 3',detectIpAddress: '192,168,1.3'),
-//   DetectDevice(detectName:'Device 4',detectIpAddress: '192,168,1.4'),
-//   DetectDevice(detectName:'Device 5',detectIpAddress: '192,168,1.5'),
-//   DetectDevice(detectName:'Device 6',detectIpAddress: '192,168,1.6'),
-//   DetectDevice(detectName:'Device 7',detectIpAddress: '192,168,1.7'),
-//   DetectDevice(detectName:'Device 8',detectIpAddress: '192,168,1.8'),
-//   DetectDevice(detectName:'Device 9',detectIpAddress: '192,168,1.9'),
-// ];
